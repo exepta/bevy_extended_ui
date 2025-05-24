ecd497a25ef113e2a9c1067c78b2c8415228fc62
exepta <genshin.exepta@outlook.com>
2025-05-24 14:25:29 +0200

    feat(#14): include new image_cache_service.rs and tested

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,9 +1,11 @@
 mod style_service;
 mod css_service;
 pub mod state_service;
+mod image_cache_service;
 
 use bevy::prelude::*;
 use crate::service::css_service::CssService;
+use crate::service::image_cache_service::ImageCacheService;
 use crate::service::state_service::StateService;
 use crate::service::style_service::StyleService;
 
@@ -11,6 +13,6 @@ pub struct ServicePlugin;
 
 impl Plugin for ServicePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((StateService, CssService, StyleService));
+        app.add_plugins((StateService, CssService, StyleService, ImageCacheService));
     }
 }
