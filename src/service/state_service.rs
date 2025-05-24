894f9e593a4ba77c92b1682023f2222f786283bf
exepta <genshin.exepta@outlook.com>
2025-05-25 01:50:22 +0200

    feat(#8): TAB logic was implemented and tested

--- src/service/state_service.rs
+++ src/service/state_service.rs
@@ -6,7 +6,10 @@ pub struct StateService;
 impl Plugin for StateService {
     fn build(&self, app: &mut App) {
         app.add_systems(Update, update_widget_states);
-        app.add_systems(Update, internal_state_check.run_if(resource_changed::<CurrentWidgetState>));
+        app.add_systems(Update, (
+            internal_state_check.run_if(resource_changed::<CurrentWidgetState>), 
+            handle_tab_focus
+        ));
     }
 }
 
@@ -42,5 +45,40 @@ fn internal_state_check(
             continue;
         }
         state.focused = false;
+    }
+}
+
+fn handle_tab_focus(
+    mut query: Query<(Entity, &mut UIWidgetState, &UIGenID)>,
+    keyboard: Res<ButtonInput<KeyCode>>
+) {
+    let mut sorted_ui_elements: Vec<_> = query.iter_mut().collect();
+    sorted_ui_elements.sort_by_key(|(_, _, id)| id.0);
+    
+    let mut any_focused = false;
+    if keyboard.just_pressed(KeyCode::Tab) {
+        let len = sorted_ui_elements.len();
+        if len == 0 {
+            return;
+        }
+
+        for i in 0..len {
+            if sorted_ui_elements[i].1.focused {
+                sorted_ui_elements[i].1.focused = false;
+
+                let next = (i + 1) % len;
+
+                if let Some(&mut (_, _, _)) = sorted_ui_elements.get_mut(next) {
+                    sorted_ui_elements[next].1.focused = true;
+                    any_focused = true;
+                }
+
+                break;
+            }
+        }
+
+        if !any_focused && len > 0 {
+            sorted_ui_elements[0].1.focused = true;
+        }
     }
 }
