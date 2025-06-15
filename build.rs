eee0895c387b292313b167938fc0707ca1b724de
exepta <genshin.exepta@outlook.com>
2025-06-15 18:17:58 +0200

    fix(#45): implements build.rs in Cargo.toml

--- build.rs
+++ build.rs
@@ -1,9 +1,25 @@
+use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() {
-    let _ = std::env::var("OUT_DIR").unwrap();
-    let target_assets = Path::new("../../assets/extended_ui/icons");
-    fs::create_dir_all(&target_assets).unwrap();
-    fs::copy("assets/extended_ui/icons/check-mark.png", target_assets.join("check-mark.png")).unwrap();
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let source_dir = PathBuf::from(manifest_dir).join("assets/extended_ui/icons");
+    
+    let target_dir = PathBuf::from("../assets/extended_ui/icons");
+    
+    fs::create_dir_all(&target_dir).expect("Failed to create target assets directory");
+    
+    for entry in fs::read_dir(&source_dir).expect("Failed to read source asset dir") {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            let file_name = path.file_name().unwrap();
+            let target_path = target_dir.join(file_name);
+            fs::copy(&path, &target_path)
+                .expect(&format!("Failed to copy asset to {:?}", target_path));
+        }
+    }
+
+    println!("cargo:rerun-if-changed=assets/extended_ui/icons");
 }
