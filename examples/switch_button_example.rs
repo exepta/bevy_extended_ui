9d242fb3ad04ab019d21a0bdfd1367fa1b775912
exepta <genshin.exepta@outlook.com>
2025-12-27 18:42:31 +0100

    switch button example

---
+++ examples/switch_button_example.rs
@@ -0,0 +1,19 @@
+use bevy_extended_ui::html::HtmlEvent;
+use bevy::prelude::*;
+use bevy_extended_ui::example_utils::make_app;
+use bevy_extended_ui::html::HtmlSource;
+use bevy_extended_ui::io::HtmlAsset;
+use bevy_extended_ui::registry::UiRegistry;
+use bevy_extended_ui::styles::CssID;
+use bevy_extended_ui_macros::html_fn;
+
+fn main() {
+    let mut app = make_app("Debug Html UI - test");
+
+    app.add_systems(Startup, |mut reg: ResMut<UiRegistry>, asset_server: Res<AssetServer>| {
+        let handle: Handle<HtmlAsset> = asset_server.load("examples/switch_button.html");
+        reg.add_and_use("switch_button_test".to_string(), HtmlSource::from_handle(handle));
+    });
+
+    app.run();
+}
